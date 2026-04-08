7d18236d82b444fef46f9d7bca1a9a07bed62c32
Nidhi Subrahmanya <nidhisubrahmanya06@gmail.com>
2026-04-08 01:14:26 -0400

    Pictures added for activities + singing done

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,13 +2,11 @@
 
 export function Footer() {
   return (
-    <footer className="py-12 mt-10 border-t border-border/50">
+    <footer className="py-12 mt-8 border-t border-border/50">
       <div className="text-center space-y-3">
+        
         <p className="text-sm text-muted-foreground">
-          Nidhi Subrahmanya @ 2026.
-        </p>
-        <p className="text-sm text-muted-foreground">
-          inspired by ian korovinsky
+          Nidhi Subrahmanya ♡ 2026.    
         </p>
       </div>
     </footer>
